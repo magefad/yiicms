64ae5ca706a283462e27cfc622ba8086cbc2281d
Ruslan Fadeev <fadeevr@gmail.com>
2012-09-04 16:21:45 +0400

    Update tinyMce extended_valid_elements , reformat config code

--- protected/config/main.php
+++ protected/config/main.php
@@ -6,19 +6,19 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 
-define('JUI-THEME','dark-hive');
+define('JUI-THEME', 'dark-hive');
 return array(
-	'basePath'			=> dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'				=> 'Fad cms',
-	'defaultController'	=> 'page/show',
-	#'homeLink'			=> '/',
-	'language'			=> 'ru',
-	'theme'				=> 'reflector',
+	'basePath'          => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+	'name'              => 'Fad cms',
+	'defaultController' => 'page/show',
+	#'homeLink'=> '/',
+	'language'          => 'ru',
+	'theme'             => 'reflector',
 	// preloading 'log' component
-	'preload'			=> array('log', 'bootstrap'),
+	'preload'           => array('log', 'bootstrap'),
 
 	// autoloading model and component classes
-	'import' => array(
+	'import'            => array(
 		'application.models.*',
 		'application.components.*',
 		// notbase
@@ -29,92 +29,88 @@ return array(
 		'application.extensions.galleria.*'
 	),
 
-	'modules' => array(
-		'gii' => array(
-			'class'			 => 'system.gii.GiiModule',
-			'password'		 => 'fad',
-		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'		 => array('127.0.0.1','::1'),
-			'generatorPaths' => array('bootstrap.gii'),
-		),
-		'rights' => array(
-			'superuserName'		 => 'Admin', // Name of the role with super user privileges.
-			'authenticatedName'	 => 'Authenticated', // Name of the authenticated user role.
-			#'userIdColumn'		 => 'id', // Name of the user id column in the database.
-			#'userNameColumn'	 => 'username', // Name of the user name column in the database.
-			'enableBizRule'		 => true, // Whether to enable authorization item business rules.
-			'enableBizRuleData'	 => false, // Whether to enable data for business rules.
-			'displayDescription' => true, // Whether to use item description instead of name.
-			'flashSuccessKey'	 => 'RightsSuccess', // Key to use for setting success flash messages.
-			'flashErrorKey'		 => 'RightsError', // Key to use for setting error flash messages.
-			#'install' 			 => true, // Whether to install rights.
-			'baseUrl' 			 => '/rights', // Base URL for Rights. Change if module is nested.
-			'layout'			 => 'rights.views.layouts.main', // Layout to use for displaying Rights.
-			'appLayout'			 => 'application.views.layouts.main', // Application layout.
-			#'cssFile'			 => 'rights.css', // Style sheet file to use for Rights.
-			'debug'				 => false, // Whether to enable debug mode.
+	'modules'           => array(
+		'gii'       => array(
+			'class'         => 'system.gii.GiiModule',
+			'password'      => 'fad',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'     => array('127.0.0.1', '::1'),
+			'generatorPaths'=> array('bootstrap.gii'),
+		), 'rights' => array(
+			'superuserName'         => 'Admin', // Name of the role with super user privileges.
+			'authenticatedName'     => 'Authenticated', // Name of the authenticated user role.
+			#'userIdColumn'     => 'id', // Name of the user id column in the database.
+			#'userNameColumn'   => 'username', // Name of the user name column in the database.
+			'enableBizRule'         => true, // Whether to enable authorization item business rules.
+			'enableBizRuleData'     => false, // Whether to enable data for business rules.
+			'displayDescription'    => true, // Whether to use item description instead of name.
+			'flashSuccessKey'       => 'RightsSuccess', // Key to use for setting success flash messages.
+			'flashErrorKey'         => 'RightsError', // Key to use for setting error flash messages.
+			#'install'               => true, // Whether to install rights.
+			'baseUrl'               => '/rights', // Base URL for Rights. Change if module is nested.
+			'layout'                => 'rights.views.layouts.main', // Layout to use for displaying Rights.
+			'appLayout'             => 'application.views.layouts.main', // Application layout.
+			#'cssFile'               => 'rights.css', // Style sheet file to use for Rights.
+			'debug'                 => false, // Whether to enable debug mode.
 		),
 	),
 
 	// application components
-	'components' => array(
-		'user' => array(
-			'class'			 => 'RWebUser',
-			#'loginUrl' 	 => '/site/login/',
-			'allowAutoLogin' => true,// enable cookie-based authentication
+	'components'        => array(
+		'user'             => array(
+			'class'          => 'RWebUser', #'loginUrl' 	 => '/site/login/',
+			'allowAutoLogin' => true, // enable cookie-based authentication
 		),
-		'image' => array(
-			'class'		=> 'ext.image.CImageComponent',#'application.helpers.image.CImageComponent',
-			'driver'	=> 'GD',// GD or ImageMagick
+		'image'            => array(
+			'class'    => 'ext.image.CImageComponent', #'application.helpers.image.CImageComponent',
+			'driver'   => 'GD', // GD or ImageMagick
 			#'params'	=> array('directory'=>'/opt/local/bin'),// ImageMagick setup path
 		),
-		'clientScript' => array(
+		'clientScript'     => array(
 			#'coreScriptPosition' => CClientScript::POS_END,
 			'packages' => array(
-				'jquery' => array(
-					'baseUrl' => '//ajax.googleapis.com/ajax/libs/jquery/1.7/',
-					'js'	  => array('jquery.min.js'),
+				'jquery'       => array(
+					'baseUrl' => '//ajax.googleapis.com/ajax/libs/jquery/1.7/', 'js'      => array('jquery.min.js'),
 				),
 				'jquery.ui' => array(
 					'baseUrl' => '//ajax.googleapis.com/ajax/libs/jqueryui/1.7/',
-					'js'	  => array('jquery-ui.min.js'),
+					'js'      => array('jquery-ui.min.js'),
 				),
 			),
 		),
-		'widgetFactory' => array(
+		'widgetFactory'    => array(
 			'widgets' => array(
-				'TinyMce' => require(dirname(__FILE__) . '/tinymce.php'),
+				'TinyMce'        => require(dirname(__FILE__) . '/tinymce.php'),
 				'CJuiDatePicker' => array( // where <WidgetName> is the name of the JUI Widget (Tabs, DatePicker, etc.). Each CJuiWidget used must be declared
-					'scriptUrl' 	 => '//ajax.googleapis.com/ajax/libs/jqueryui/1.7',
-					'i18nScriptFile' => 'i18n/jquery-ui-i18n.min.js',
-					#'theme'		 => JUI-THEME,
-					'themeUrl'		 => '//ajax.googleapis.com/ajax/libs/jqueryui/1.7/themes',
+					'scriptUrl'        => '//ajax.googleapis.com/ajax/libs/jqueryui/1.7',
+					'i18nScriptFile'   => 'i18n/jquery-ui-i18n.min.js',
+					#'theme'           => JUI-THEME,
+					'themeUrl'         => '//ajax.googleapis.com/ajax/libs/jqueryui/1.7/themes',
 				),
 			),
 		),
-		'urlManager'  => require(dirname(__FILE__) . '/urlManager.php'),
-		'db'		  => require(dirname(__FILE__) . '/db.php'),
-		'authManager' => array(
-			'class'			  => 'RDbAuthManager',
+		'urlManager'       => require(dirname(__FILE__) . '/urlManager.php'),
+		'db'               => require(dirname(__FILE__) . '/db.php'),
+		'authManager'      => array(
+			'class'              => 'RDbAuthManager',
 			#default is Auth#'defaultRoles' => array('Guest'),
-			'itemTable'		  => 'fad_auth_item',
-			'itemChildTable'  => 'fad_auth_item_child',
-			'assignmentTable' => 'fad_auth_assignment',
-			'rightsTable'	  => 'fad_rights',
+			'itemTable'          => 'fad_auth_item',
+			'itemChildTable'     => 'fad_auth_item_child',
+			'assignmentTable'    => 'fad_auth_assignment',
+			'rightsTable'        => 'fad_rights',
 			#'connectionID'	  => 'db',
 		),
-		'errorHandler' => array(
-            'errorAction' => 'site/error',// use 'site/error' action to display errors
-        ),
-		'cache' => array(
+		'errorHandler'     => array(
+			'errorAction' => 'site/error', // use 'site/error' action to display errors
+		),
+		'cache'            => array(
 			'class' => 'CFileCache',
 		),
-		'log' => array(
+		'log'              => array(
 			'class'  => 'CLogRouter',
 			'routes' => array(
 				array(
-					'class'  => 'CFileLogRoute',
-					'levels' => 'error, warning, info',
+					'class'  => 'CFileLogRoute', 'levels' => 'error, warning, info',
 				),
 				// uncomment the following to show log messages on web pages
 				/*
@@ -130,18 +126,18 @@ return array(
 				),*/
 			),
 		),
-		'bootstrap' => array(
-			'class'			=> 'ext.bootstrap.components.Bootstrap', // assuming you extracted bootstrap under extensions
-			'responsiveCss'	=> true,
+		'bootstrap'        => array(
+			'class'            => 'ext.bootstrap.components.Bootstrap',
+			// assuming you extracted bootstrap under extensions
+			'responsiveCss'    => true,
 		),
 	),
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
-	'params' => array(
+	'params'            => array(
 		// this is used in contact page
-		'adminEmail' => 'fad@itrade-rus.ru',
-		'cacheTime'	 => 3600,
-		/* page */
-		'index'		 => 'index',
+		'adminEmail'=> 'fad@itrade-rus.ru',
+		'cacheTime' => 3600,
+		'index'     => 'index',
 	),
 );
--- protected/config/tinymce.php
+++ protected/config/tinymce.php
@@ -1,26 +1,25 @@
 <?php
 return array(
-	'language' => 'ru',
+	'language'           => 'ru',
 	'fileManager' => array(
-		'class'				=> 'ext.elFinder.TinyMceElFinder',
-		'connectorRoute'	=> 'elfinder/connector',
+		'class'          => 'ext.elFinder.TinyMceElFinder', 'connectorRoute' => 'elfinder/connector',
 	),
 	'compressorRoute' => 'tinyMce/compressor',
 	#'spellcheckerRoute' => 'tinyMce/spellchecker',
-	'settings' => array(
-		'doctype'						=> '<!DOCTYPE html>',
-		'body_class'					=> 'container',
-		'width'							=> '100%',
-		'plugins'						=> 'autolink,lists,pagebreak,layer,table,save,advhr,advimage,advlink,inlinepopups,insertdatetime,preview,media,searchreplace,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist',
-		'theme_advanced_buttons1'		=> "save,|,|,undo,redo,|,|,pastetext,pasteword,|,|,attribs,styleprops,charmap,|,|,removeformat,visualaid,|,|,|,hr,advhr,|,|,fullscreen,code,|",
-		'theme_advanced_buttons2'		=> "styleselect,formatselect,fontselect,fontsizeselect,|,tablecontrols,|",
-		'theme_advanced_buttons3'		=> "bold,italic,underline,strikethrough,|,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,outdent,indent,blockquote,|,link,unlink,anchor,image,cleanup,|,media,|,forecolor,backcolor,|,insertdate,inserttime,|,preview,|,spellchecker",
-		'theme_advanced_buttons4'		=> '',
-		#'theme_advanced_buttons4' 		=> "insertlayer,moveforward,movebackward,absolute,|,cite,abbr,acronym,del,ins|,visualchars,nonbreaking,template,pagebreak",
-		'theme_advanced_toolbar_location'=> "top",
-		'theme_advanced_toolbar_align'	=> "left",
-		'external_link_list_url'		=> '/page/MceListUrl',
-		'relative_urls'					=> true,
+	'settings'           => array(
+		'doctype'                          => '<!DOCTYPE html>',
+		'extended_valid_elements'          => 'iframe[id|class|title|style|align|frameborder|height|longdesc|marginheight|marginwidth|name|scrolling|src|width]',
+		'body_class'                       => 'container',
+		'width'                            => '100%',
+		'plugins'                          => 'autolink,lists,pagebreak,layer,table,save,advhr,advimage,advlink,inlinepopups,insertdatetime,preview,media,searchreplace,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist',
+		'theme_advanced_buttons1'          => "save,|,|,undo,redo,|,|,pastetext,pasteword,|,|,attribs,styleprops,charmap,|,|,removeformat,visualaid,|,|,|,hr,advhr,|,|,fullscreen,code,|",
+		'theme_advanced_buttons2'          => "styleselect,formatselect,fontselect,fontsizeselect,|,tablecontrols,|",
+		'theme_advanced_buttons3'          => "bold,italic,underline,strikethrough,|,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,outdent,indent,blockquote,|,link,unlink,anchor,image,cleanup,|,media,|,forecolor,backcolor,|,insertdate,inserttime,|,preview,|,spellchecker",
+		#'theme_advanced_buttons4'         => "insertlayer,moveforward,movebackward,absolute,|,cite,abbr,acronym,del,ins|,visualchars,nonbreaking,template,pagebreak",
+		'theme_advanced_toolbar_location'  => "top",
+		'theme_advanced_toolbar_align'     => "left",
+		'external_link_list_url'           => '/page/MceListUrl',
+		'relative_urls'                    => true,
 	),
 )
 ?>
