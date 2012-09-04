f4f6108e5ecbfd913dbfc313ec90f96d2b827455
Ruslan Fadeev <fadeevr@gmail.com>
2012-09-04 16:21:45 +0400

    Config main.php reformat code

--- protected/config/main.php
+++ protected/config/main.php
@@ -18,10 +18,10 @@ return array(
 	'preload'           => array('log', 'bootstrap'),
 
 	// autoloading model and component classes
-	'import'            => array(
+	'import' => array(
 		'application.models.*',
 		'application.components.*',
-		// notbase
+		// not base
 		'application.helpers.*',
 		'application.modules.rights.RightsModule',
 		'application.modules.rights.rights.*',
@@ -29,48 +29,50 @@ return array(
 		'application.extensions.galleria.*'
 	),
 
-	'modules'           => array(
-		'gii'       => array(
+	'modules' => array(
+		'gii' => array(
 			'class'         => 'system.gii.GiiModule',
 			'password'      => 'fad',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'     => array('127.0.0.1', '::1'),
 			'generatorPaths'=> array('bootstrap.gii'),
-		), 'rights' => array(
-			'superuserName'         => 'Admin', // Name of the role with super user privileges.
-			'authenticatedName'     => 'Authenticated', // Name of the authenticated user role.
-			#'userIdColumn'     => 'id', // Name of the user id column in the database.
-			#'userNameColumn'   => 'username', // Name of the user name column in the database.
-			'enableBizRule'         => true, // Whether to enable authorization item business rules.
-			'enableBizRuleData'     => false, // Whether to enable data for business rules.
-			'displayDescription'    => true, // Whether to use item description instead of name.
-			'flashSuccessKey'       => 'RightsSuccess', // Key to use for setting success flash messages.
-			'flashErrorKey'         => 'RightsError', // Key to use for setting error flash messages.
-			#'install'               => true, // Whether to install rights.
-			'baseUrl'               => '/rights', // Base URL for Rights. Change if module is nested.
-			'layout'                => 'rights.views.layouts.main', // Layout to use for displaying Rights.
-			'appLayout'             => 'application.views.layouts.main', // Application layout.
-			#'cssFile'               => 'rights.css', // Style sheet file to use for Rights.
-			'debug'                 => false, // Whether to enable debug mode.
+		),
+		'rights' => array(
+			'superuserName'      => 'Admin', // Name of the role with super user privileges.
+			'authenticatedName'  => 'Authenticated', // Name of the authenticated user role.
+			#'userIdColumn'      => 'id', // Name of the user id column in the database.
+			#'userNameColumn'    => 'username', // Name of the user name column in the database.
+			'enableBizRule'      => true, // Whether to enable authorization item business rules.
+			'enableBizRuleData'  => false, // Whether to enable data for business rules.
+			'displayDescription' => true, // Whether to use item description instead of name.
+			'flashSuccessKey'    => 'RightsSuccess', // Key to use for setting success flash messages.
+			'flashErrorKey'      => 'RightsError', // Key to use for setting error flash messages.
+			#'install'           => true, // Whether to install rights.
+			'baseUrl'            => '/rights', // Base URL for Rights. Change if module is nested.
+			'layout'             => 'rights.views.layouts.main', // Layout to use for displaying Rights.
+			'appLayout'          => 'application.views.layouts.main', // Application layout.
+			#'cssFile'           => 'rights.css', // Style sheet file to use for Rights.
+			'debug'              => false, // Whether to enable debug mode.
 		),
 	),
 
 	// application components
-	'components'        => array(
-		'user'             => array(
+	'components' => array(
+		'user' => array(
 			'class'          => 'RWebUser', #'loginUrl' 	 => '/site/login/',
 			'allowAutoLogin' => true, // enable cookie-based authentication
 		),
-		'image'            => array(
-			'class'    => 'ext.image.CImageComponent', #'application.helpers.image.CImageComponent',
-			'driver'   => 'GD', // GD or ImageMagick
-			#'params'	=> array('directory'=>'/opt/local/bin'),// ImageMagick setup path
+		'image' => array(
+			'class'   => 'ext.image.CImageComponent', #'application.helpers.image.CImageComponent',
+			'driver'  => 'GD', // GD or ImageMagick
+			#'params' => array('directory'=>'/opt/local/bin'),// ImageMagick setup path
 		),
-		'clientScript'     => array(
+		'clientScript' => array(
 			#'coreScriptPosition' => CClientScript::POS_END,
 			'packages' => array(
-				'jquery'       => array(
-					'baseUrl' => '//ajax.googleapis.com/ajax/libs/jquery/1.7/', 'js'      => array('jquery.min.js'),
+				'jquery' => array(
+					'baseUrl' => '//ajax.googleapis.com/ajax/libs/jquery/1.7/',
+					'js'      => array('jquery.min.js'),
 				),
 				'jquery.ui' => array(
 					'baseUrl' => '//ajax.googleapis.com/ajax/libs/jqueryui/1.7/',
@@ -78,39 +80,39 @@ return array(
 				),
 			),
 		),
-		'widgetFactory'    => array(
+		'widgetFactory' => array(
 			'widgets' => array(
 				'TinyMce'        => require(dirname(__FILE__) . '/tinymce.php'),
 				'CJuiDatePicker' => array( // where <WidgetName> is the name of the JUI Widget (Tabs, DatePicker, etc.). Each CJuiWidget used must be declared
-					'scriptUrl'        => '//ajax.googleapis.com/ajax/libs/jqueryui/1.7',
-					'i18nScriptFile'   => 'i18n/jquery-ui-i18n.min.js',
-					#'theme'           => JUI-THEME,
-					'themeUrl'         => '//ajax.googleapis.com/ajax/libs/jqueryui/1.7/themes',
+					'scriptUrl'      => '//ajax.googleapis.com/ajax/libs/jqueryui/1.7',
+					'i18nScriptFile' => 'i18n/jquery-ui-i18n.min.js',
+					#'theme'         => JUI-THEME,
+					'themeUrl'       => '//ajax.googleapis.com/ajax/libs/jqueryui/1.7/themes',
 				),
 			),
 		),
-		'urlManager'       => require(dirname(__FILE__) . '/urlManager.php'),
-		'db'               => require(dirname(__FILE__) . '/db.php'),
-		'authManager'      => array(
-			'class'              => 'RDbAuthManager',
+		'urlManager'  => require(dirname(__FILE__) . '/urlManager.php'),
+		'db'          => require(dirname(__FILE__) . '/db.php'),
+		'authManager' => array(
+			'class'           => 'RDbAuthManager',
 			#default is Auth#'defaultRoles' => array('Guest'),
-			'itemTable'          => 'fad_auth_item',
-			'itemChildTable'     => 'fad_auth_item_child',
-			'assignmentTable'    => 'fad_auth_assignment',
-			'rightsTable'        => 'fad_rights',
-			#'connectionID'	  => 'db',
+			'itemTable'       => 'fad_auth_item',
+			'itemChildTable'  => 'fad_auth_item_child',
+			'assignmentTable' => 'fad_auth_assignment',
+			'rightsTable'     => 'fad_rights',
+			#'connectionID'  => 'db',
 		),
-		'errorHandler'     => array(
+		'errorHandler' => array(
 			'errorAction' => 'site/error', // use 'site/error' action to display errors
 		),
-		'cache'            => array(
+		'cache' => array(
 			'class' => 'CFileCache',
 		),
-		'log'              => array(
+		'log' => array(
 			'class'  => 'CLogRouter',
 			'routes' => array(
 				array(
-					'class'  => 'CFileLogRoute', 'levels' => 'error, warning, info',
+					'class' => 'CFileLogRoute', 'levels' => 'error, warning, info',
 				),
 				// uncomment the following to show log messages on web pages
 				/*
@@ -118,26 +120,20 @@ return array(
 					'class' => 'CWebLogRoute',
 				),
 				*/
-				//профайлер запросов к базе данных, на продакшн серверах рекомендуется отключить
-				/*array(
-					'class'        => 'application.modules.yupe.extensions.db_profiler.DbProfileLogRoute',
-					'countLimit'   => 1, // How many times the same query should be executed to be considered inefficient
-					'slowQueryMin' => 0.01, // Minimum time for the query to be slow
-				),*/
 			),
 		),
-		'bootstrap'        => array(
-			'class'            => 'ext.bootstrap.components.Bootstrap',
+		'bootstrap' => array(
+			'class'         => 'ext.bootstrap.components.Bootstrap',
 			// assuming you extracted bootstrap under extensions
-			'responsiveCss'    => true,
+			'responsiveCss' => true,
 		),
 	),
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
-	'params'            => array(
+	'params' => array(
 		// this is used in contact page
-		'adminEmail'=> 'fad@itrade-rus.ru',
-		'cacheTime' => 3600,
-		'index'     => 'index',
+		'adminEmail' => 'fad@itrade-rus.ru',
+		'cacheTime'  => 3600,
+		'index'      => 'index',
 	),
 );
