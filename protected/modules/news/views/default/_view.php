777c52b2d1892a52787a140d35c312841a50747a
Ruslan Fadeev <fadeevr@gmail.com>
2012-09-13 17:02:10 +0400

    Update news index template

--- protected/modules/news/views/default/_view.php
+++ protected/modules/news/views/default/_view.php
@@ -1,18 +1,11 @@
-<?php
-/** @var $data News */
-/** @var $thumbnailUrl string Thumbnail Image URL */
-$thumbnailUrl = News::model()->findByPk($data->id)->getThumbnailUrl();
-?>
-<div style="margin-bottom: 20px;padding-bottom: 40px;clear: both">
-	<h6><span class="label"><?php echo $data->date;?></span>
-		<?php echo CHtml::link($data->title, array('default/show', 'slug' => $data->slug));?></h6>
+<div class="span12 news-item">
 	<?php
-	if ( $thumbnailUrl )
-	{
-		echo CHtml::link(
-			CHtml::image($thumbnailUrl, $data->title, array('style' => 'float:left;padding: 5px 10px 10px 0;width:100px')),
-			array('default/show', 'slug' => $data->slug)
-		);
-	}
-	echo $data->body_cut;?>
+	/** @var $data News */
+	/** @var $thumbnailUrl string Thumbnail Image URL */
+	if ( $thumbnailUrl = News::model()->findByPk($data->id)->getThumbnailUrl() )
+		echo CHtml::link(CHtml::image($thumbnailUrl, $data->title), array('default/show', 'slug' => $data->slug));
+	?>
+	<h5><?php echo CHtml::link($data->title, array('default/show', 'slug' => $data->slug));?></h5>
+	<span class="label"><?php echo $data->date;?></span>
+	<?php echo $data->body_cut;?>
 </div>
--- protected/modules/news/views/default/index.php
+++ protected/modules/news/views/default/index.php
@@ -2,14 +2,38 @@
 $this->breadcrumbs = array(
 	Yii::t('news', 'Новости')
 );
-
+?>
+<h2 style="margin:0">Новости</h2>
+<style>
+	.news-item {
+		margin-bottom: 30px;
+	}
+	.news-item img {
+		float: left;
+		padding: 3px 10px 0 0;
+		width: 100px;
+	}
+	.news-item h5 {
+		line-height: 16px;
+		margin: 0;
+	}
+	.news-item .label {
+		font-size: 11px;
+	}
+	.news-item p {
+		margin: 5px 0 0 0;
+	}
+</style>
+<?php
 $this->menu = array(
 	array('label' => Yii::t('news', 'Новости')),
 	array('icon' => 'list-alt', 'label' => Yii::t('news', 'Управление'), 'url' => array('admin')),
 	array('icon' => 'th-list white', 'label' => Yii::t('news', 'Показать анонсами'), 'url' => array('/news/default/index')),
 	array('icon' => 'file', 'label' => Yii::t('news', 'Добавить'), 'url' => array('create')),
 );
-
+?>
+<div class="row">
+<?php
 /** @var $dataProvider CDataProvider */
 $this->widget('bootstrap.widgets.TbListView', array(
 	'dataProvider'       => $dataProvider,
@@ -20,3 +44,4 @@ $this->widget('bootstrap.widgets.TbListView', array(
 	),
 	'htmlOptions'  => array('style' => 'padding-top:0'),
 )); ?>
+</div>
