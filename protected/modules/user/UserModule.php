da8165a7ebecf9ee6ad61f1f7b910d0bdd4991e1
Ruslan Fadeev <fadeevr@gmail.com>
2012-10-01 17:57:43 +0400

    Added EAuth extension

--- protected/modules/user/UserModule.php
+++ protected/modules/user/UserModule.php
@@ -23,7 +23,21 @@ class UserModule extends WebModule
             array(
                 'user.models.*',
                 'user.components.*',
-                'user.extensions.*'
+                'user.extensions.eoauth.*',
+                'user.extensions.eoauth.lib.*',
+                'user.extensions.lightopenid.*',
+                'user.extensions.eauth.*',
+                'user.extensions.eauth.services.*',
+                'user.extensions.eauth.custom_services.*',
+            )
+        );
+        //load EAuth components
+        Yii::app()->setComponents(
+            array(
+                'loid'  => array(
+                    'class' => 'user.extensions.lightopenid.loid',
+                ),
+                'eauth' => require('config' . DIRECTORY_SEPARATOR . 'eauth.php')
             )
         );
     }
