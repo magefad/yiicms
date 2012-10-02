16844d4158730ac0b8108f7fccba62178d6932af
Ruslan Fadeev <fadeevr@gmail.com>
2012-10-02 15:45:23 +0400

    Fix show error message

--- protected/modules/user/controllers/account/LoginAction.php
+++ protected/modules/user/controllers/account/LoginAction.php
@@ -29,7 +29,7 @@ class LoginAction extends CAction
                     $authIdentity->redirect();
                 } else {
                     // close popup window and redirect to cancelUrl
-                    $authIdentity->cancel();
+                    #$authIdentity->cancel();
                 }
             }
             $authIdentity->redirect('/user/account/login');
