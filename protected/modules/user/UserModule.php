24c21a85cb37eaa95e601f6d8f25ca88d05b9355
Ruslan Fadeev <fadeevr@gmail.com>
2012-10-03 17:42:33 +0400

    client_id, client_sercret of social networks now load from module "social"

--- protected/modules/user/UserModule.php
+++ protected/modules/user/UserModule.php
@@ -40,13 +40,48 @@ class UserModule extends WebModule
                 'ext.mail.YiiMailMessage',
             )
         );
+
+        /**
+         * Load Services Client_id and Client_secret and push it to array
+         * If Open Id (yandex and google) push it anywhere.. (no need client_id and client_secret)
+         * @var $socialModule SocialModule
+         */
+        $socialModule = Yii::app()->getModule('social');
+        $servicesClasses = array('VKontakte', 'Facebook', 'Yandex', 'Google', 'Twitter', 'Mailru', 'Moikrug', 'Odnoklassniki');
+        $services = array();
+
+        foreach ($servicesClasses as $service) {
+            $serviceClass        = 'Custom' . $service . 'Service';
+            $serviceName         = strtolower($service);
+            $serviceClientId     = $serviceName . 'ClientId';
+            $serviceClientSecret = $serviceName . 'ClientSecret';
+            unset($services[$service]);
+
+            if (isset($socialModule->$serviceClientId) && !empty($socialModule->$serviceClientId)) {
+                $services[$serviceName] = array(
+                    'class'         => $serviceClass,
+                    'client_id'     => $socialModule->$serviceClientId,
+                    'client_secret' => $socialModule->$serviceClientSecret,
+                );
+            } else if (in_array($serviceName, array('yandex', 'google'))) {//Open ID is good... no need client_id and client_secret
+                $services[$serviceName] = array(
+                    'class'         => $serviceClass,
+                );
+            }
+        }
+
         //load EAuth components
         Yii::app()->setComponents(
             array(
                 'loid'  => array(
                     'class' => 'user.extensions.lightopenid.loid',
                 ),
-                'eauth' => require('config' . DIRECTORY_SEPARATOR . 'eauth.php'),
+                'eauth' => array(
+                    'class'       => 'user.extensions.eauth.EAuth',
+                    'popup'       => true,
+                    'cacheExpire' => 3600,
+                    'services'    => $services
+                ),
                 'mail'  => array(
                     'class'         => 'ext.mail.YiiMail',
                     'transportType' => 'php',
