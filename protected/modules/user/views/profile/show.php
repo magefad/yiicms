9d8a15da5199d5bbb9280254c1704c24e7210bd4
Ruslan Fadeev <fadeevr@gmail.com>
2012-10-03 18:25:20 +0400

    Show profile view

--- protected/modules/user/views/profile/show.php
+++ protected/modules/user/views/profile/show.php
@@ -10,15 +10,35 @@ $this->breadcrumbs = array(
 );
 $this->widget('bootstrap.widgets.TbAlert');
 ?>
-<div class="span4 well">
+<div class="span5 offset3 well">
     <div class="row">
         <div class="span1">
-            <img src="http://critterapp.pagodabox.com/img/user.jpg" alt="" class="thumbnail">
+            <?php if (!empty($user->avatar)) {
+            echo CHtml::image($user->avatar);
+        } else if (($user->getAvatar())) {
+            echo $user->getAvatar();
+        } else {
+            echo CHtml::image('http://www.gravatar.com/avatar/00000000000000000000000000000000');
+        }
+            ?>
         </div>
         <div class="span3">
-            <p><?php echo CHtml::encode($user->username); ?></p>
-            <strong><p><?php echo CHtml::encode($user->firstname); ?></p></strong>
-            <span class=" badge badge-warning">8 messages</span> <span class=" badge badge-info">15 followers</span>
+            <p><?php echo $user->username; ?></p>
+            <?php if (!empty($user->firstname) || !empty($user->lastname)): ?>
+            <p style="font-weight:bold">
+                <?php if (!empty($user->firstname)): ?>
+                <?php echo CHtml::encode($user->firstname); ?>
+                <?php endif; ?>
+                <?php if (!empty($user->lastname)): ?>
+                <?php echo CHtml::encode($user->lastname); ?>
+                <?php endif; ?>
+            </p>
+            <?php endif; ?>
+            <?php if (!is_null($user->last_visit)): ?>
+            <p>
+            <?php echo Yii::t('user', 'Был на сайте') . ' ' . $user->last_visit; ?>
+            </p>
+            <?php endif; ?>
         </div>
     </div>
 </div>
