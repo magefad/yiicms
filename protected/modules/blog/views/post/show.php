9cba2717bc21cceffbdee07e47e703547611b566
Ruslan Fadeev <fadeevr@gmail.com>
2012-10-10 18:32:46 +0400

    Double content

--- protected/modules/blog/views/post/show.php
+++ protected/modules/blog/views/post/show.php
@@ -17,11 +17,9 @@ $this->breadcrumbs = array(
     <?php echo $model->content; ?>
     <i class="icon-user"></i> <?php echo $model->createUser->username; ?>
     | <i class="icon-calendar"></i> <?php echo $model->publish_time; ?>
-    | <i class="icon-comment"></i> <?php echo $model->getCommentCount() . ' ' . Yii::t('blog', 'Comment|Comments|Comments', $model->getCommentCount()); ?>
-    <!--| <i class="icon-share"></i> <a href="#">39 Shares</a> -->
     | <i class="icon-tags"></i> <?php echo Yii::t('blog', 'Tags'); ?>:
     <?php foreach ($tags as $tag): ?>
     <?php echo CHtml::link($tag, array('/blog/post/tag', 'tag' => $tag), array('class' => 'label label-info')); ?>
     <?php endforeach; ?>
 </div>
-<?php $this->renderPartial('application.modules.comment.views.default.commentList', array('model'=> $model)); ?>
+<?php $this->renderPartial('application.modules.comment.views.default.commentList', array('model' => $model)); ?>
