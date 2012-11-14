811c0ce743142abf24d4b9d6c4c5c87eb596f7b3
Ruslan Fadeev <fadeevr@gmail.com>
2012-11-14 18:37:07 +0400

    Updated last news template

--- protected/modules/news/widgets/views/news.php
+++ protected/modules/news/widgets/views/news.php
@@ -1,15 +1,20 @@
 <?php
-/** @var $news News */
+/**
+ * @var $news News
+ * @var $new News
+ */
 ?>
 <div class='portlet'>
-    <div class='portlet-decoration'>
-        <div class='portlet-title'>Новости</div>
-    </div>
     <div class='portlet-content'>
         <ul class="unstyled">
             <?php foreach ($news as $new): ?>
-            <li><?php echo CHtml::link($new->title . ' »', array('/news/default/show', 'slug' => $new->slug));?></li>
+                <li>
+                    <small class="text-success"><?php echo $new->date; ?></small><br />
+                    <?php echo CHtml::link($new->title, array('/news/default/show', 'slug' => $new->slug));?>
+                    <?php echo $new->content_short; ?>
+                </li>
             <?php endforeach;?>
         </ul>
+        <?php echo CHtml::link('Все новости', array('/news'), array('class' => 'pull-right')); ?>
     </div>
 </div>
