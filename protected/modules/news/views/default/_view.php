f818d763025072d40babc360d7466cefd5b3e9cf
Ruslan Fadeev <fadeevr@gmail.com>
2012-11-15 23:23:04 +0400

    News views update

--- protected/modules/news/views/default/_view.php
+++ protected/modules/news/views/default/_view.php
@@ -4,16 +4,14 @@
  * @var $thumbnailUrl string Thumbnail Image URL
  */
 ?>
-<div class="span12 news-item">
+<div class="news-item">
     <?php
     if ($thumbnailUrl = News::model()->findByPk($data->id)->getThumbnailUrl()) {
-        echo CHtml::link(
-            CHtml::image($thumbnailUrl, $data->title),
-            array('default/show', 'slug' => $data->slug)
-        );
+        echo CHtml::link(CHtml::image($thumbnailUrl, $data->title), array('default/show', 'slug' => $data->slug));
     }
     ?>
-    <h5><?php echo CHtml::link($data->title, array('default/show', 'slug' => $data->slug));?></h5>
+    <h2><?php echo CHtml::link($data->title, array('default/show', 'slug' => $data->slug));?></h2>
     <span class="label"><?php echo $data->date;?></span>
     <?php echo $data->content_short;?>
 </div>
+<div style="clear: both">&nbsp;</div>
--- protected/modules/news/views/default/index.php
+++ protected/modules/news/views/default/index.php
@@ -1,29 +1,34 @@
 <?php
+/**
+ * @var $this Controller
+ * @var $dataProvider CDataProvider
+ */
 $this->breadcrumbs = array(
 	Yii::t('news', 'Новости')
 );
+$css = <<<CSS
+.news-item {
+    margin-bottom: 30px;
+}
+.news-item img {
+    float: left;
+    padding-right: 10px;
+}
+.news-item h2 {
+    font-size: 1.5em;
+    line-height: 1;
+    margin: 0.2em;
+}
+.news-item .label {
+    font-size: 11px;
+}
+.news-item p {
+    margin-top: 5px;
+}
+CSS;
+Yii::app()->clientScript->registerCss('news', $css);
 ?>
-<h2 style="margin:0">Новости</h2>
-<style>
-	.news-item {
-		margin-bottom: 30px;
-	}
-	.news-item img {
-		float: left;
-		padding: 3px 10px 0 0;
-		width: 100px;
-	}
-	.news-item h5 {
-		line-height: 16px;
-		margin: 0;
-	}
-	.news-item .label {
-		font-size: 11px;
-	}
-	.news-item p {
-		margin: 5px 0 0 0;
-	}
-</style>
+<h1 style="margin:0"><?php echo Yii::t('news', 'Новости'); ?></h1>
 <?php
 $this->menu = array(
 	array('label' => Yii::t('news', 'Новости')),
@@ -31,23 +36,17 @@ $this->menu = array(
 	array('icon' => 'th-list white', 'label' => Yii::t('news', 'Показать анонсами'), 'url' => array('/news/default/index')),
 	array('icon' => 'file', 'label' => Yii::t('news', 'Добавить'), 'url' => array('create')),
 );
-?>
-<div class="row">
-<?php
-/**
- * @var $this Controller
- * @var $dataProvider CDataProvider
- */
-    $this->widget(
-        'bootstrap.widgets.TbListView',
-        array(
-            'dataProvider'       => $dataProvider,
-            'itemView'           => '_view',
-            'sortableAttributes' => array(
-                'title',
-                'date',
-            ),
-            'htmlOptions'        => array('style' => 'padding-top:0'),
-        )
-    ); ?>
-</div>
+$this->widget(
+    'bootstrap.widgets.TbListView',
+    array(
+        'dataProvider'       => $dataProvider,
+        'enableHistory'      => true,
+        'itemView'           => '_view',
+        'sortableAttributes' => array(
+            'title',
+            'date',
+        ),
+        'htmlOptions'        => array('style' => 'padding-top:0'),
+        'template'           => '{items}{pager}{summary}{sorter}',
+    )
+); ?>
