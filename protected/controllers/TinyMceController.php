92569c439ff8a27a80d02c99653a2588489b75f1
Ruslan Fadeev <fadeevr@gmail.com>
2012-11-24 20:19:51 +0400

    Added spellchecker

--- protected/controllers/TinyMceController.php
+++ protected/controllers/TinyMceController.php
@@ -19,16 +19,16 @@ class TinyMceController extends CController
     public function actions()
     {
         return array(
-            'compressor' => array(
+            'compressor'   => array(
                 'class'    => 'TinyMceCompressorAction',
                 'settings' => array(
                     'compress'   => true,
                     'disk_cache' => true,
                 )
             ),
-            #'spellchecker' => array(
-            #'class' => 'TinyMceSpellcheckerAction',
-            #),
+            'spellchecker' => array(
+                'class' => 'TinyMceSpellcheckerAction',
+            ),
         );
     }
 }
