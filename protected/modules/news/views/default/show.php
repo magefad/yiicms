2e548ea3b89a26d5c82d02b9fb172ad81b8187ff
Ruslan Fadeev <fadeevr@gmail.com>
2012-12-04 12:46:14 +0400

    News show fix

--- protected/modules/news/views/default/show.php
+++ protected/modules/news/views/default/show.php
@@ -13,13 +13,9 @@ $this->breadcrumbs = array(
 <div class="post">
     <h2><?php echo $model->title?></h2>
     <?php if ($model->image) {
-    echo CHtml::image(
-        $model->getImageUrl(),
-        $model->title,
-        array('style' => 'float:left; margin: 0 15px 15px 0')
-    );
-}?>
-    <?php echo $model->content; ?>
+        echo CHtml::image($model->getImageUrl(), $model->title, array('style' => 'float:left; margin: 0 15px 15px 0'));
+    }
+    echo empty($model->content) ? $model->content_short : $model->content; ?>
     <div class="nav">
         <span class="label"><?php echo $model->date; ?></span> <i class="icon-user"></i>
         <b><?php echo $model->author->username?></b>
