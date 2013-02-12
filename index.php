f0fd01959bd71d803e0b60a9733d02f8dda491f1
Ruslan Fadeev <fadeevr@gmail.com>
2013-02-12 17:13:42 +0400

    Turn on YII_DEBUG if dev mode

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ $yii = dirname(__FILE__) . '/../../../framework/yii.php';
 if (!file_exists(dirname(__FILE__) . '/protected/config/db.php')) {
     $config = 'install.php';
 } elseif ($_SERVER['SERVER_ADDR'] == '127.0.0.1') {
+    define('YII_DEBUG', true);
+    define('YII_TRACE_LEVEL', 3);
     $config = 'dev.php';
 } else {
     $yii    = dirname(__FILE__) . '/../private/framework/yii.php';
