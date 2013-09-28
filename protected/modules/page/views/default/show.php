0042b8a48ea9b8b5f71f1299fb6402801608c57f
Ruslan Fadeev <fadeevr@gmail.com>
2013-09-28 16:32:32 +0400

    Added show catalog in page module

--- protected/modules/page/views/default/show.php
+++ protected/modules/page/views/default/show.php
@@ -4,9 +4,25 @@
  * Date: 16.07.12
  * Time: 17:14
  * @var $this Controller
+ * @var $page Page
  */
-/** @var $page Page */
 echo $this->decodeWidgets($page->content);
+if (Yii::app()->hasModule('catalog')) {
+    if (!is_null($page->type) && $page->type == Page::TYPE_CATALOG) {
+        Yii::import('catalog.models.Good');//@todo why not found?
+        foreach ($page->goods as $good) {
+            $this->renderPartial('application.modules.catalog.views.good.' . Yii::app()->getModule('catalog')->template, array('good' => $good));
+        }
+    } else {
+        echo CHtml::tag('ul', array('class' => 'nav nav-tabs nav-stacked'));
+        foreach ($page->children as $children) {
+            if (!is_null($children->type) && $children->type == Page::TYPE_CATALOG) {
+                echo '<li>' . CHtml::link($children->title, array('/page/default/show', 'slug' => $children->slug)) . '</li>';
+            }
+        }
+        echo CHtml::closeTag('ul');
+    }
+}
 if (!empty($navigation)):?>
 <ul class="pager">
 <?php if (isset($navigation['prev'])):?>
@@ -20,10 +36,4 @@ if (!empty($navigation)):?>
     </li>
 <?php endif;?>
 </ul>
-<?php endif;
-if (!is_null($page->type) && $page->type == Page::TYPE_CATALOG && Yii::app()->hasModule('catalog')) {
-    Yii::import('catalog.models.Good');//@todo why not found?
-    foreach ($page->goods as $good) {
-        $this->renderPartial('application.modules.catalog.views.good.' . Yii::app()->getModule('catalog')->template, array('good' => $good));
-    }
-}
+<?php endif;?>
