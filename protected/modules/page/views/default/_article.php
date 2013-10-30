c3277154be02412ff507b5327743865d29f08dc3
Ruslan Fadeev <fadeevr@gmail.com>
2013-10-31 00:07:46 +0400

    Updated article view template

--- protected/modules/page/views/default/_article.php
+++ protected/modules/page/views/default/_article.php
@@ -1,4 +1,8 @@
-<?php /** @var Page $data */
+<?php
+/**
+ * @var Page $data
+ * @var int $index
+ */
 if (!($index % 3)):
     if($index):?>
     </ul>
@@ -9,10 +13,10 @@ if (!($index % 3)):
             <div class="thumbnail">
                 <?= CHtml::link(CHtml::image($data->getThumbnailUrl(), $data->name), array('/page/default/show', 'slug' => $data->slug)) ?>
                 <div class="caption">
-                    <h3><?= $data->name ?></h3>
-                    <small><?= Yii::app()->dateFormatter->formatDateTime($data->create_time, 'long', 'short'); ?></small>
+                    <h3><?= CHtml::link($data->name, array('/page/default/show', 'slug' => $data->slug)) ?></h3>
+                    <div class="date"><?= Yii::app()->dateFormatter->formatDateTime($data->create_time, 'long', 'short'); ?></div>
                     <p><?= $data->annotation ?></p>
-                    <?= CHtml::link(Yii::t('PageModule.page', 'Continue reading') . '<i class="icon-circle-arrow-right"></i>', array('/page/default/show', 'slug' => $data->slug), array('class' => 'btn btn-mini')) ?>
+                    <?= CHtml::link(Yii::t('PageModule.page', 'Full article') . ' <i class="icon-circle-arrow-right"></i>', array('/page/default/show', 'slug' => $data->slug), array('class' => 'btn btn-mini')) ?>
                 </div>
             </div>
         </li>
